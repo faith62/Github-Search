75371329d699d9e3d650365539d6da38dcc4e2d3
faith62 <phifeichemu@gmail.com>
2022-04-19 00:25:33 +0300

    add service component

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { User } from '../user-class/user';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Repository } from '../repository-class/repository';
+
+
 
 
 @Injectable({ // tell Angular that this class is a service
@@ -6,5 +12,16 @@ import { Injectable } from '@angular/core';
 })
 export class GithubService {
 
-  constructor() { }
+  user:User;
+  reporitory:Repository;
+  repoData=[];
+  newUserData: any = [];
+  showInput:boolean;
+  showData:boolean;
+
+  constructor(private http: HttpClient) { 
+    this.user = new User("",0,"","",new Date(),new Date(),"");
+  		this.repository = new Repository("","","",new Date(),"","","",new Date()); 	
+
+  }
 }
