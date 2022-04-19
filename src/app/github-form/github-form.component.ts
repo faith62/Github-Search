2a1c05823a5dc9f2a1b5a4867613fdee0c864662
faith62 <phifeichemu@gmail.com>
2022-04-19 10:34:00 +0300

    updated gitresult comonent on what to display

--- src/app/github-form/github-form.component.ts
+++ src/app/github-form/github-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';//so users can use HttpClient app
+import { NgForm } from '@angular/forms';
+import { User } from '../user-class/user';
+import { GithubService } from '../github-service/github.service';
 
 @Component({
   selector: 'app-github-form',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubFormComponent implements OnInit {
 
-  constructor() { }
+  user!: User;
+  username!: string; //from github username input
+//from github username input
+  githubService:GithubService;
+  public showInput = true;
+  public showData = false;
 
-  ngOnInit(): void {
+  submitUsername() {
+		this.githubService.getUserData(this.username);
+    this.showInput = false;
+    this.showData = true;
+	}
+
+  showUserInput(hideInput: boolean) {
+    this.showInput = hideInput;
+    this.showData = false;
+  }
+
+ 
+	constructor(githubService:GithubService) { 
+		this.githubService = githubService;
+	}
+
+
+
+ 
+
+  ngOnInit(){
   }
 
 }
--- src/app/github-results/github-results.component.ts
+++ src/app/github-results/github-results.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { GithubService } from '../github-service/github.service';
+import { NgForm } from '@angular/forms';
+import { User } from '../user-class/user';
 
 @Component({
   selector: 'app-github-results',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubResultsComponent implements OnInit {
 
-  constructor() { }
+  user:User | undefined;
+  repoDetails = [];
+  githubService: GithubService;
+  hideInput: boolean = false;
 
-  ngOnInit(): void {
+  constructor(githubService:GithubService) {
+    this.githubService =githubService;
+  }
+
+  @Output() toggleBack = new EventEmitter();
+
+  goBack(){
+    this.hideInput = true;
+    this.toggleBack.emit(this.hideInput);
+  }
+
+
+
+  ngOnInit(){
+    this.user = this.githubService.user;
+    this.repoDetails = this.githubService.repoData
   }
 
 }
