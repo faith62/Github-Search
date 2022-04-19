cbfc801e62910aa87a266cc21f69c3dc290d546c
faith62 <phifeichemu@gmail.com>
2022-04-19 15:26:49 +0300

    add background image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { User } from './user-class/user';
 import { Repository } from './repository-class/repository';
 import { GithubResultsComponent } from './github-results/github-results.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 
 const routes: Routes = [
   // defined the path and component for each route
-  {path: 'github', component:GithubComponent},
-  {path: 'Result', component:GithubResultsComponent},
+  {path: 'github', component:GithubFormComponent},
+  {path: 'result', component:GithubResultsComponent},
   {path: 'user', component:User},
   {path: 'repos', component:Repository},
 
