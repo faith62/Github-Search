54a3be2bf18cca919f20e1235aa7438ce09190ce
faith62 <phifeichemu@gmail.com>
2022-04-19 12:08:07 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubComponent } from './github/github.component';
+import { User } from './user-class/user';
+import { Repository } from './repository-class/repository';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // defined the path and component for each route
+  {path: 'github', component:GithubComponent},
+  {path: 'user', component:User},
+  {path: 'repos', component:Repository},
+
+  { path: '', redirectTo:"/github", pathMatch:"full"},
+  // {path: '**', component:NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
